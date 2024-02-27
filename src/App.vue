d3489ad6e438d82e4eb8748d7444c2f770b256fa
lingongbing <462441355@qq.com>
2024-02-27 14:20:28 +0800

    * init

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,30 @@
-<script setup lang="ts">
-import { onLaunch, onShow, onHide } from "@dcloudio/uni-app";
-onLaunch(() => {
-  console.log("App Launch");
-});
-onShow(() => {
-  console.log("App Show");
-});
-onHide(() => {
-  console.log("App Hide");
-});
-</script>
-<style></style>
+<template>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view/>
+</template>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
-import { createSSRApp } from "vue";
-import App from "./App.vue";
-export function createApp() {
-  const app = createSSRApp(App);
-  return {
-    app,
-  };
-}
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+createApp(App).use(store).use(router).mount('#app')
